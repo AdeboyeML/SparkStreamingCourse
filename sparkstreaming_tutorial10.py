fe1468e6cbf9a1e7fe551b63fcf8e7b2f688294e
Adeniyi Adeboye <oluomo_lade@yahoo.com>
2021-08-01 20:27:03 -0400

    Update sparkstreaming_tutorial10.py

--- sparkstreaming_tutorial10.py
+++ sparkstreaming_tutorial10.py
@@ -2,7 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
 from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
 
-# define schema in spark
+# this is a manually created schema - before Spark 3.0.0, schema inference is not automatic
+
 redisMessageSchema = StructType(
     [
         StructField("key", StringType()),
@@ -22,63 +23,87 @@ redisMessageSchema = StructType(
     ]
 )
 
-# reservation schema
-reservationJSONSchema = StructType([
-    StructField("reservationId", StringType()),
+# TO-DO: create a StructType for the Customer schema for the following fields:
+# {"customerName":"Frank Aristotle","email":"Frank.Aristotle@test.com","phone":"7015551212","birthDay":"1948-01-01","accountNumber":"750271955","location":"Jordan"}
+customerJSONSchema = StructType([
     StructField("customerName", StringType()),
-    StructField("truckNumber", StringType()),
-    StructField("reservationDate", StringType())
+    StructField("email", StringType()),
+    StructField("phone", StringType()),
+    StructField("birthDay", StringType()),
+    StructField("accountNumber", StringType()),
+    StructField("location", StringType())
 ])
-
-# payment schema
-paymentJSONSchema = StructType([
-    StructField("reservationId", StringType()),
-    StructField("customerName", StringType()),
-    StructField("date", StringType()),
-    StructField("amount", StringType())
+# TO-DO: create a StructType for the CustomerLocation schema for the following fields:
+# {"accountNumber":"814840107","location":"France"}
+customerLocationSchema = StructType([
+    StructField("accountNumber", StringType()),
+    StructField("location", StringType())
 ])
-
-spark = SparkSession.builder.appName("reservation-payment").getOrCreate()
+# TO-DO: create a spark session, with an appropriately named application name
+spark = SparkSession.builder.appName("customer-record").getOrCreate()
+#TO-DO: set the log level to WARN
 spark.sparkContext.setLogLevel("WARN")
-
-# vehicle status
-# read stream from kafka by subscribing to kafka topic
-redisServerRawStreamingDF = spark\
+#TO-DO: read the redis-server kafka topic as a source into a streaming dataframe with the bootstrap server localhost:9092, configuring the stream to read the earliest messages possible                                    
+redisServerRawStreamingDF = spark \
     .readStream\
     .format("kafka")\
     .option("kafka.bootstrap.servers", "localhost:9092")\
     .option("subscribe", "redis-server")\
     .option("startingOffsets", "earliest")\
     .load()
-
+#TO-DO: using a select expression on the streaming dataframe, cast the key and the value columns from kafka as strings, and then select them
 redisServerStreamingDF = redisServerRawStreamingDF.selectExpr("cast(key as string) key", "cast(value as string) value")
-
+#TO-DO: using the redisMessageSchema StructType, deserialize the JSON from the streaming dataframe 
 redisServerStreamingDF.withColumn("value", from_json("value", redisMessageSchema))\
     .select(col("value.*"))\
     .createOrReplaceTempView("RedisData")
-
+# TO-DO: create a temporary streaming view called "RedisData" based on the streaming dataframe
+# it can later be queried with spark.sql
 zSetEntriesEncodedStreamingDF = spark.sql("select key, zSetEntries[0].element as redisEvent from RedisData")
-
+#TO-DO: using spark.sql, select key, zSetEntries[0].element as redisEvent from RedisData
 zSetEntriesDecodedStreamingDF1 = zSetEntriesEncodedStreamingDF.withColumn("redisEvent", unbase64(zSetEntriesEncodedStreamingDF.redisEvent).cast("string"))
 zSetEntriesDecodedStreamingDF2 = zSetEntriesEncodedStreamingDF.withColumn("redisEvent", unbase64(zSetEntriesEncodedStreamingDF.redisEvent).cast("string"))
+#TO-DO: from the dataframe use the unbase64 function to select a column called redisEvent with the base64 decoded JSON, and cast it to a string
+zSetEntriesDecodedStreamingDF1.filter(col("redisEvent").contains("birthDay"))
+zSetEntriesDecodedStreamingDF2.filter(~col("redisEvent").contains("birthDay"))
 
-zSetEntriesDecodedStreamingDF1.filter(col("redisEvent").contains("reservationDate"))
-zSetEntriesDecodedStreamingDF2.filter(~col("redisEvent").contains("reservationDate"))
+#TO-DO: using spark.sql select accountNumber as customerAccountNumber, location as homeLocation, birthDay from Customer where birthDay is not null
+zSetEntriesDecodedStreamingDF1.withColumn("customer", from_json("redisEvent", customerJSONSchema))\
+    .select(col("customer.*"))\
+    .createOrReplaceTempView("Customer")
 
-zSetEntriesDecodedStreamingDF1.withColumn("reservation", from_json("redisEvent", reservationJSONSchema))\
-    .select(col("reservation.*"))\
-    .createOrReplaceTempView("Reservation")
-
-zSetEntriesDecodedStreamingDF2.withColumn("payment", from_json("redisEvent", paymentJSONSchema))\
-    .select(col("payment.*"))\
-    .createOrReplaceTempView("Payment")
+zSetEntriesDecodedStreamingDF2.withColumn("customerLocation", from_json("redisEvent", customerLocationSchema))\
+    .select(col("customerLocation.*"))\
+    .createOrReplaceTempView("customerLocation")
 
 # query data from temp view
-reservationStreamingDF = spark.sql("select * from Reservation where reservationDate is not null")
-paymentStreamingDF = spark.sql("select reservationId as paymentReservationId, date as paymentDate, amount as paymentAmount from Payment")
+customerStreamingDF = spark.sql("select accountNumber as customerAccountNumber, location as homeLocation, birthDay from Customer")
+relevantCustomerFieldsStreamingDF = customerStreamingDF.select("customerAccountNumber", "homeLocation", split(customerStreamingDF.birthDay,"-").getItem(0).alias("birthYear"))
 
-paymentReservationStreamingDF = reservationStreamingDF.join(paymentStreamingDF, 
-                                                            expr(""" reservationId = paymentReservationId
+
+customerLocationStreamingDF = spark.sql("select * from customerLocation")
+
+customerHomeStreamingDF = relevantCustomerFieldsStreamingDF.join(customerLocationStreamingDF, 
+                                                            expr(""" customerAccountNumber = accountNumber
                                                             """)
                                                            )
-paymentReservationStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
+customerHomeStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
+
+# TO-DO: write the stream to the console, and configure it to run indefinitely
+# can you find the customer(s) who are traveling out of their home countries?
+# When calling the customer, customer service will use their birth year to help
+# establish their identity, to reduce the risk of fraudulent transactions.
+# +---------------------+-----------+---------------------+------------+---------+
+# |locationAccountNumber|   location|customerAccountNumber|homeLocation|birthYear|
+# +---------------------+-----------+---------------------+------------+---------+
+# |            982019843|  Australia|            982019843|   Australia|     1943|
+# |            581813546|Phillipines|            581813546| Phillipines|     1939|
+# |            202338628|Phillipines|            202338628|       China|     1944|
+# |             33621529|     Mexico|             33621529|      Mexico|     1941|
+# |            266358287|     Canada|            266358287|      Uganda|     1946|
+# |            738844826|      Egypt|            738844826|       Egypt|     1947|
+# |            128705687|    Ukraine|            128705687|      France|     1964|
+# |            527665995|   DR Congo|            527665995|    DR Congo|     1942|
+# |            277678857|  Indonesia|            277678857|   Indonesia|     1937|
+# |            402412203|   DR Congo|            402412203|    DR Congo|     1945|
+# +---------------------+-----------+---------------------+------------+---------+
